00ec74df6c983475b3af5198791b7a9970b8d9ff
Adam Duke <adam.v.duke@gmail.com>
2014-05-14 22:47:36 -0400

    apparently this needs a name

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ license           "MIT"
 description       "add swap to an ubuntu server"
 version           "0.0.1"
 recipe            "add_swap", "add swap to an ubuntu server"
+name              "add_swap"
 
 %w{ ubuntu }.each do |os|
   supports os
